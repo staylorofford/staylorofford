d01a603a420bc06d7ab5854560cd2a7c50a1a5d2
samto <samto@hutl15444.gns.cri.nz>
2019-03-28 21:33:35 +1300

    Add spreadsheet conversion and lat,lon,depth time series plots to sequence_analysis.py

--- duty_tools/sequence_analysis.py
+++ duty_tools/sequence_analysis.py
@@ -10,6 +10,7 @@ import math
 import matplotlib.pyplot as plt
 from obspy.io.quakeml.core import Unpickler
 import pycurl
+import subprocess
 quakeml_reader = Unpickler()
 
 
@@ -45,7 +46,7 @@ def curl(curlstr):
     return buffer.getvalue()
 
 
-def parse_csv(file_path):
+def parse_csv(file_path, data_columns_out):
 
     """
     Parse contents of sequence csv created by GHAs into data lists
@@ -63,42 +64,37 @@ def parse_csv(file_path):
             except:
                 continue
 
-            if cols[2] == 'Yes':
+            if cols[2] == 'Y':
                 data_lists[1].append(True)
-            elif cols[2] == 'No':
+            elif cols[2] == 'N':
                 data_lists[1].append(False)
 
-            if cols[3] == 'Yes':
+            if cols[3] == 'Y':
                 data_lists[2].append(True)
             else:
                 data_lists[2].append(False)
 
             data_lists[3].append(cols[4])
 
-            if cols[5] == 'Yes':
-                data_lists[4].append(True)
-            else:
-                data_lists[4].append(False)
+            try:
+                data_lists[4].append(float(cols[5]))
+            except:
+                data_lists[4].append(float('nan'))
 
             try:
                 data_lists[5].append(float(cols[6]))
             except:
                 data_lists[5].append(float('nan'))
 
-            try:
-                data_lists[6].append(float(cols[7]))
-            except:
-                data_lists[6].append(float('nan'))
-
-            if cols[8] == 'Yes':
-                data_lists[7].append(True)
-            elif cols[8] == 'No':
-                data_lists[7].append(False)
+            if cols[7] == 'Y':
+                data_lists[6].append(True)
+            elif cols[7] == 'N':
+                data_lists[6].append(False)
 
             try:
-                data_lists[8].append(float(cols[9]))
+                data_lists[7].append(float(cols[8]))
             except:
-                data_lists[8].append(float('nan'))
+                data_lists[7].append(float('nan'))
 
     return data_lists
 
@@ -166,20 +162,31 @@ def distance(p1, p2):
 # Data column details
 
 data_columns_in = ['date', 'GLKZ arrival time (UTC)', 'autolocation?', 'GHA event created?', 'eventID',
-                   'reached mainland?', 'MLv', 'mB', 'USGS location?', 'USGS Mww', 'Comments']
-data_columns_out = data_columns_in[1:10]
+                   'MLv', 'mB', 'USGS location?', 'USGS Mww', 'Comments']
+data_columns_out = data_columns_in[1:9]
 
 # Parse arguments
 
 parser = argparse.ArgumentParser()
-parser.add_argument('csv_file', type=str, help='Path to csv-version of Kermadec sequence spreadsheet')
+parser.add_argument('-csv', type=str, help='Path to csv-version of Kermadec sequence spreadsheet')
+parser.add_argument('-xlsx', type=str, help='Path to excel spreadsheet of Kermadec sequence')
 args = parser.parse_args()
 
-file_path = args.csv_file
+file_path = args.csv
+
+# If the input is a spreadsheet, convert to a csv
+if not file_path:
+    if args.xlsx:
+        p = subprocess.Popen('ssconvert ' + args.xlsx + ' sequence_analysis.csv', shell=True)
+        p.wait()
+        file_path = 'sequence_analysis.csv'
+    else:
+        print('You need to give an argument, use -h flag to see which options are available.')
+        exit()
 
 # Parse data from csv file
 
-data_lists = parse_csv(file_path)
+data_lists = parse_csv(file_path, data_columns_out)
 
 # Build automatic and manual detection data lists
 
@@ -199,18 +206,11 @@ for i in range(len(data_lists[0])):
     except:  # This fails when trying to get detection data that doesn't exist, but only after getting time data
         continue
 
-# Get details of largest event (mB) in sequence
-
-max_mag = 0
-for i in range(len(data_lists[6])):
-    if data_lists[6][i] > max_mag:
-        max_mag, max_time, max_n = data_lists[6][i], data_lists[0][i], i
-
 # Calculate magnitude distribution and plot it
 
 colors = ['g', 'r', 'b']
 labels = ['all detections', 'manual detections', 'automatic detections']
-magnitude_indices = [5, 6]
+magnitude_indices = [4, 5]
 
 fig = plt.figure(figsize=(9, 6))
 mag_values, value_count = [[] for i in range(len(magnitude_indices))], [[] for i in range(len(magnitude_indices))]
@@ -242,6 +242,13 @@ plt.subplots_adjust(left=0.13, right=0.98, bottom=0.12, top=0.9, hspace=0.35)
 fig.suptitle('Earthquake Magnitude Distribution', y=0.95)
 plt.show()
 
+# Get details of largest event (mB) in sequence
+
+max_mag = 0
+for i in range(len(data_lists[magnitude_indices[1]])):
+    if data_lists[magnitude_indices[1]][i] > max_mag:
+        max_mag, max_time, max_n = data_lists[magnitude_indices[1]][i], data_lists[0][i], i
+
 # Plot cumulative event number and cumulative moment release over time
 
 fig = plt.figure(figsize=(9, 6))
@@ -256,12 +263,12 @@ plt.ylabel('cumulative\nevent count', labelpad=10)
 ax2 = plt.subplot('212', sharex=ax1)
 for i in range(len(dll)):
     cumulative_moments, mB_times = [], []
-    for j in range(len(dll[i][6])):  # Calculate moment for each event
-        if str(dll[i][6][j]) != 'nan':
+    for j in range(len(dll[i][magnitude_indices[1]])):  # Calculate moment for each event
+        if str(dll[i][magnitude_indices[1]][j]) != 'nan':
             try:
-                cumulative_moments.append(cumulative_moments[-1] + 10 ** (1.5 * (dll[i][6][j] + 6.06)))
+                cumulative_moments.append(cumulative_moments[-1] + 10 ** (1.5 * (dll[i][magnitude_indices[1]][j] + 6.06)))
             except:
-                cumulative_moments.append(10 ** (1.5 * (dll[i][6][j] + 6.06)))
+                cumulative_moments.append(10 ** (1.5 * (dll[i][magnitude_indices[1]][j] + 6.06)))
             mB_times.append(dll[i][0][j])
     plt.plot(mB_times, cumulative_moments, color=colors[i], label=labels[i])
     if i == 0:  # Give the cumulative moment magnitude
@@ -307,8 +314,8 @@ for j in range(len(magnitude_indices)):
         ax2 = plt.subplot(str(len(magnitude_indices)) + '1' + str(j + 1), sharex=ax1)
 
     for k in range(len(dll[0][magnitude_indices[j]])):
-        if str(dll[0][magnitude_indices[j]][k]) != 'nan' and str(dll[0][8][k]) != 'nan':
-            diff = dll[0][magnitude_indices[j]][k] - dll[0][8][k]
+        if str(dll[0][magnitude_indices[j]][k]) != 'nan' and str(dll[0][-1][k]) != 'nan':
+            diff = dll[0][magnitude_indices[j]][k] - dll[0][-1][k]
             plt.scatter(dll[0][magnitude_indices[j]][k], diff, color='k', s=9)
     plt.ylabel(data_columns_out[magnitude_indices[j]] + ' - USGS Mww', labelpad=10)
     plt.xlabel(data_columns_out[magnitude_indices[j]], labelpad=10)
@@ -335,7 +342,8 @@ plt.show()
 
 # Get event details from GeoNet FDSN
 
-event_fields = ['eventID', 'origin_time', 'MLv', 'mB', 'longitude', 'latitude', 'depth']
+event_fields = ['eventID', 'origin_time', 'MLv', 'mB', 'longitude', 'latitude', 'depth',
+                'relative_origin_time', 'relative_origin_distance']
 event_details = [[] for i in range(len(event_fields))]
 
 for eventID in dll[0][3]:
@@ -354,15 +362,46 @@ for eventID in dll[0][3]:
         elif magnitude.magnitude_type == 'mB':
             event_details[3].append(magnitude.mag)
 
-# Calculate time, distance of events from largest event in sequence, then plot it
+# Calculate time, distance of events from largest event in sequence
 
 times, distances = [], []
 for i in range(len(event_details[0])):
-    times.append((event_details[1][i].datetime - event_details[1][max_n].datetime).total_seconds() / 86400)
-    distances.append(distance([event_details[4][i], event_details[5][i]],
+    event_details[7].append((event_details[1][i].datetime - event_details[1][max_n].datetime).total_seconds() / 86400)
+    event_details[8].append(distance([event_details[4][i], event_details[5][i]],
                               [event_details[4][max_n], event_details[5][max_n]]))
+
+# Plot event details
+
 fig = plt.figure(figsize=(9, 6))
-plt.scatter(times, distances, color='k', s=9)
+EQ_loc_data = [event_details[4], event_details[5], event_details[6]]
+EQ_loc_labels = ['longitude', 'latitude', 'depth']
+for j in range(len(EQ_loc_data)):
+    if j == 0:
+        ax1 = plt.subplot(str(len(EQ_loc_data)) + '1' + str(j + 1))
+    else:
+        ax2 = plt.subplot(str(len(EQ_loc_data)) + '1' + str(j + 1), sharex=ax1)
+
+    plt.scatter(event_details[7], EQ_loc_data[j], color='k', s=9)
+    plt.axvline(0, color='k', linestyle='--', alpha=0.5)
+    plt.xlabel('days since largest earthquake', labelpad=10)
+    plt.ylabel(EQ_loc_labels[j], labelpad=10)
+
+# Only have x-axis tick labels on last subplot
+
+for ax in plt.gcf().axes:
+    try:
+        ax.label_outer()
+    except:
+        pass
+
+plt.subplots_adjust(left=0.13, right=0.98, bottom=0.12, top=0.9)
+fig.suptitle('Earthquake Locations over Time', y=0.95)
+plt.show()
+
+# Plot relative distance of events over time
+
+fig = plt.figure(figsize=(9, 6))
+plt.scatter(event_details[7], event_details[8], color='k', s=9)
 plt.axvline(0, color='k', linestyle='--', alpha=0.5)
 plt.xlabel('days since largest earthquake', labelpad=10)
 plt.ylabel('distance from largest earthquake (km)', labelpad=10)
