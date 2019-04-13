e1700e738e3a2a55a5a41d0c1b061ff007fc810a
Sam Taylor-Offord <samto@hutl15444.gns.cri.nz>
2019-04-13 18:24:59 +1200

    Try generating path by matching line segment vertices

--- traffic_calculations/calculate_commute_path.py
+++ traffic_calculations/calculate_commute_path.py
@@ -9,10 +9,10 @@ def parse_path(path_file, path_columns):
 
     """
     :param path_file: path to path file
-    :return: nested lists of path point coordinates, list of path point vertices
+    :return: list of line segment vertices on path
     """
 
-    path_points = [[], [], []]
+    point_lists = [[] for n in range(len(path_columns))]
     vertex_list = []
     with open(path_file, 'r') as openfile:
         header = '0'
@@ -29,12 +29,19 @@ def parse_path(path_file, path_columns):
             # Extract data into lists
 
             cols = row.split(',')
+
+            try:  # Check vertex is useful data
+                float(cols[len(path_columns) - 1])
+            except:  # Line identificator missing
+                continue
+
             vertex_list.append([])
+
             for n in range(len(path_columns)):
-                path_points[n].append(float(cols[n]))
+                point_lists[n].append(float(cols[n]))
                 vertex_list[-1].append(float(cols[n]))
 
-    return path_points, vertex_list
+    return point_lists, vertex_list
 
 
 def distance(p1, p2):
@@ -72,61 +79,133 @@ if __name__ == "__main__":
 
     print('\nParsing path vertex data...\n')
 
-    path_columns = ['X', 'Y', 'Z']
+    path_columns = ['X', 'Y', 'Z', 'fid']
+    points_lists = [[], []]
     vertex_lists = [[], []]
     for m in range(len(path_files)):
-        vertex_lists[m] = parse_path(path_files[m], path_columns)
+        points_lists[m], vertex_lists[m] = parse_path(path_files[m], path_columns)
+
+    # Order vertices into a commute path
+
+    end_points = []
+    end_point_indices = []
+    for m in range(len(vertex_lists)):
+
+        # Find line segment end points for the path
+        for n in range(len(vertex_lists[m])):
+            for k in range(len(vertex_lists[m])):
+                if n != k and distance(vertex_lists[m][n], vertex_lists[m][k]) == 0:
+                    end_points.append(vertex_lists[m][n][:-1])
+                    end_point_indices.append(n)
+
+        # Find along-line direction of indices for first line segment on path
+
+        initial_idx = end_point_indices[0]
+        path_points = [vertex_lists[m][initial_idx]]
+        if vertex_lists[m][initial_idx + 1][3] == vertex_lists[m][initial_idx][3]:
+            sign = 1
+        elif vertex_lists[m][initial_idx - 1][3] == vertex_lists[m][initial_idx][3]:
+            sign = -1
+
+        # Build the path in the direction of the first line segment
+
+        idx = initial_idx
+        while idx != 0 or idx != len(vertex_lists[m]):
+            idx += sign
+            if vertex_lists[m][idx][:-1] in end_points:
+                # Jump to the end point of the new line
+                # ISSUE: poping seems to be removing required values to have this indexing method work!
+                print(idx)
+                end_points.pop(end_point_indices.index(idx))
+                end_point_indices.pop(end_point_indices.index(idx))
+                print(end_points.index(vertex_lists[m][idx][:-1]))
+                idx = end_points.index(vertex_lists[m][idx][:-1])
+                path_points.append(vertex_lists[m][idx])
+                if vertex_lists[m][idx + 1][3] == vertex_lists[m][idx][3]:
+                    sign = 1
+                elif vertex_lists[m][idx - 1][3] == vertex_lists[m][idx][3]:
+                    sign = -1
+            else:
+                path_points.append(vertex_lists[m][idx])
+
+        # Build the path in the opposite direction
+
+        idx = initial_idx[0]
+        while idx != 0 or idx != len(vertex_lists[m]):
+            if vertex_lists[m][idx][:-1] in end_points:
+                # Jump to the end point of the new line
+                end_points.pop(end_point_indices.index(idx))
+                end_point_indices.pop(end_point_indices.index(idx))
+                idx = end_points.index(vertex_lists[m][idx][:-1])
+                path_points.append(vertex_lists[m][idx])
+                if vertex_lists[m][idx + 1][3] == vertex_lists[m][idx][3]:
+                    sign = 1
+                elif vertex_lists[m][idx - 1][3] == vertex_lists[m][idx][3]:
+                    sign = -1
+            else:
+                path_points.append(vertex_lists[m][idx])
+
+        for n in range(len(vertex_lists[m])):
+            plt.scatter(vertex_lists[m][n][0], vertex_lists[m][n][1], color='k')
+            plt.text(vertex_lists[m][n][0], vertex_lists[m][n][1], str(n))
+        plt.show()
+
+
+
+            # Find which way the line approaches the end point
+
+
+
 
     # Calculate path through vertices such that each vertex is on the path and the path has the
     # minimum possible distance
-
-    print('\nSorting path vertices into path order...\n')
-
-    for m in range(len(vertex_lists)):
-        vertex_list_permutations = permutations(vertex_lists[m])
-        path_lengths = []
-        path_length_sums = []
-        n = 0
-        for permutation in vertex_list_permutations:
-            print('Calculating path length of permutation ' + str(n))
-            n += 1
-            # Calculate distance along path
-            path_length = []
-            for k in range(1, len(permutation)):
-                path_length.append(distance(permutation[k - 1], permutation[k]))
-            print('Path length is ' + str(sum(path_length)))
-            path_lengths.append(path_length)
-            path_length_sums.append(sum(path_length))
-        print(path_lengths)
-        print(min(path_length_sums))
-
-        # Build sorted point list from the permutation with minimum path length
-
-        print('\nBuilding vertex coordinate list for path...\n')
-
-        idx = path_length_sums.index(min(path_length_sums))
-        sorted_point_list = [[], [], []]
-        for n in range(len(vertex_list_permutations[idx])):
-            for k in range(len(vertex_list_permutations[idx][n])):
-                sorted_point_list[k].append(vertex_list_permutations[idx][n][k])
-
-        print('\nPlotting path vertices...\n')
-
-        # Show the operator the sorted vertices
-
-        plt.scatter(sorted_point_list[0], sorted_point_list[1])
-        for k in range(len(sorted_point_list[0])):
-            plt.text(sorted_point_list[0][k], sorted_point_list[1][k], str(k))
-        plt.show()
-
-        # Write the path and distances to file
-
-        with open('sorted_' + path_files[m], 'w') as outfile:
-            outfile.write('X,Y,Z,distance\n')
-        with open('sorted_' + path_files[m], 'a') as outfile:
-            for k in range(len(sorted_point_list[0])):
-                outfile.write(str(sorted_point_list[0][k]) + ',' +
-                              str(sorted_point_list[1][k]) + ',' +
-                              str(sorted_point_list[2][k]) + ',' +
-                              str(path_lengths[idx]) + '\n')
-
+    #
+    # print('\nSorting path vertices into path order...\n')
+    #
+    # for m in range(len(vertex_lists)):
+    #     vertex_list_permutations = permutations(vertex_lists[m])
+    #     path_lengths = []
+    #     path_length_sums = []
+    #     n = 0
+    #     for permutation in vertex_list_permutations:
+    #         print('Calculating path length of permutation ' + str(n))
+    #         n += 1
+    #         # Calculate distance along path
+    #         path_length = []
+    #         for k in range(1, len(permutation)):
+    #             path_length.append(distance(permutation[k - 1], permutation[k]))
+    #         print('Path length is ' + str(sum(path_length)))
+    #         path_lengths.append(path_length)
+    #         path_length_sums.append(sum(path_length))
+    #     print(path_lengths)
+    #     print(min(path_length_sums))
+    #     idx = path_length_sums.index(min(path_length_sums))
+    #
+    #     # Build sorted point list from the permutation with minimum path length
+    #
+    #     print('\nBuilding vertex coordinate list for path...\n')
+    #
+    #     sorted_point_list = [[], [], []]
+    #     for n in range(len(vertex_list_permutations[idx])):
+    #         for k in range(len(vertex_list_permutations[idx][n])):
+    #             sorted_point_list[k].append(vertex_list_permutations[idx][n][k])
+    #
+    #     print('\nPlotting path vertices...\n')
+    #
+    #     # Show the operator the sorted vertices
+    #
+    #     plt.scatter(sorted_point_list[0], sorted_point_list[1])
+    #     for k in range(len(sorted_point_list[0])):
+    #         plt.text(sorted_point_list[0][k], sorted_point_list[1][k], str(k))
+    #     plt.show()
+    #
+    #     # Write the path and distances to file
+    #
+    #     with open('sorted_' + path_files[m], 'w') as outfile:
+    #         outfile.write('X,Y,Z,distance\n')
+    #     with open('sorted_' + path_files[m], 'a') as outfile:
+    #         for k in range(len(sorted_point_list[0])):
+    #             outfile.write(str(sorted_point_list[0][k]) + ',' +
+    #                           str(sorted_point_list[1][k]) + ',' +
+    #                           str(sorted_point_list[2][k]) + ',' +
+    #                           str(path_lengths[idx]) + '\n')
