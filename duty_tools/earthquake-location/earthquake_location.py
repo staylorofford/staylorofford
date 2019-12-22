19a2805a15f658bf46093f055a1a337b9fd5307b
Sam Taylor-Offord <samto@hutl15444.gns.cri.nz>
2019-12-22 22:39:51 +1300

    Add untested gridsearch location routine to earthquake-location code.

--- duty_tools/earthquake-location/earthquake_location.py
+++ duty_tools/earthquake-location/earthquake_location.py
@@ -18,7 +18,7 @@ def calculate_tt(grid_point, site_location, velocity_model):
     :param grid_point: x,y,z position of a point in the grid
     :param site_location: x,y,z position of a site in the grid
     :param velocity_model: velocity model as parsed from velocity file
-    :return: travel time between grid point and site location in seconds
+    :return: p-wave and s-wave travel times between grid point and site location in seconds
     """
 
     # Find which layer the grid point sits in, this being the bottom layer to use in ray tracing
@@ -28,12 +28,14 @@ def calculate_tt(grid_point, site_location, velocity_model):
 
     # Perform ray tracing to find the travel time between the grid point and site location for the first arriving ray
     horizontal_proximities = []
-    travel_times = []
+    p_travel_times = []
+    s_travel_times = []
     for psi in range(1, 180):
 
         # Initialise ray tracing parameters
         n = nmax
-        travel_time = 0
+        p_travel_time = 0
+        s_travel_time = 0
         current_angle = psi
 
         # Collapse the 3D positions of the two points into the plane containing both points and the vertical axis
@@ -57,7 +59,8 @@ def calculate_tt(grid_point, site_location, velocity_model):
             # Increase the travel time by the time taken for the ray to traverse the hypotenuse of the
             # triangle with acute angle psi and opposite dz
 
-            travel_time += (dz / math.sin(math.radians(current_angle))) / velocity_model[n][2]
+            p_travel_time += (dz / math.sin(math.radians(current_angle))) / velocity_model[n][2]
+            s_travel_time += (dz / math.sin(math.radians(current_angle))) / velocity_model[n][3]
 
             # Calculate horizontal distance traversed by ray over dz
             dh = dz / math.tan(math.radians(current_angle))
@@ -75,7 +78,8 @@ def calculate_tt(grid_point, site_location, velocity_model):
                 # minimum horizontal distance will be the best solution to the ray tracing problem and its travel
                 # time is taken as the travel time of a ray from the grid point to the site.
                 horizontal_proximities.append(abs(hpos))
-                travel_times.append(travel_time)
+                p_travel_times.append(p_travel_time)
+                s_travel_times.append(s_travel_time)
                 break
 
             # If not, adjust current angle to that of the refracted ray in the new velocity layer
@@ -91,9 +95,10 @@ def calculate_tt(grid_point, site_location, velocity_model):
     # The travel time between the grid point and the site is that of the ray which is closest to the site when it
     # reaches the depth of the site.
 
-    travel_time = travel_times[horizontal_proximities.index(min(horizontal_proximities))]
+    p_travel_time = p_travel_times[horizontal_proximities.index(min(horizontal_proximities))]
+    s_travel_time = s_travel_times[horizontal_proximities.index(min(horizontal_proximities))]
 
-    return travel_time
+    return p_travel_time, s_travel_time
 
 
 def generate_tt_grid(xmin, xmax, ymin, ymax, zmin, zmax, xstep, ystep, zstep, network_data, velocity_model):
@@ -139,10 +144,10 @@ def generate_tt_grid(xmin, xmax, ymin, ymax, zmin, zmax, xstep, ystep, zstep, ne
             for k in range(len(gridz)):
                 grid_points[i][j][k] = [gridx[i], gridy[j], gridz[k]]
                 for m in range(len(site_grid_positions)):
-                    grid_points[i][j][k].append(calculate_tt(grid_points[i][j][k], site_grid_positions[m],
+                    grid_points[i][j][k].extend(calculate_tt(grid_points[i][j][k], site_grid_positions[m],
                                                              velocity_model))
 
-    return grid_points
+    return grid_points, grid_header
 
 
 # Parse arguments
@@ -150,9 +155,8 @@ def generate_tt_grid(xmin, xmax, ymin, ymax, zmin, zmax, xstep, ystep, zstep, ne
 parser = argparse.ArgumentParser()
 parser.add_argument('--arrival-time-file', type=str, help='Comma-separated file containing event arrival times '
                                                           'details. First row is header with columns: '
-                                                          'Event #,site1_P,site1_S,site2_P,...,siteN_S. '
+                                                          'site1_P,site1_S,site2_P,...,siteN_S. '
                                                           'All other rows contain arrival time data as: '
-                                                          'event number (e.g. 1),'
                                                           'P wave arrival time at given site in form '
                                                           '"YYYY-MM-DD HH:MM:SS" (UTC),'
                                                           'S wave arrival time at given site in form '
@@ -172,11 +176,12 @@ parser.add_argument('--network-file', type=str, help='Comma-separated file conta
                                                      'Coordinate system is not important, so long as it is cartesian.')
 parser.add_argument('--velocity-model', type=str, help='Comma-separated file containing velocity model. '
                                                        'First row is header with columns: '
-                                                       'upper_depth,lower_depth,velocity. '
+                                                       'upper_depth,lower_depth,p_velocity,s_velocity. '
                                                        'All other rows contain site details as: '
                                                        'upper depth (km, +ve direction is down),'
                                                        'lower depth (km, +ve direction is down),'
-                                                       'velocity (km/s).')
+                                                       'p-wave velocity (km/s),'
+                                                       's-wave velocity (km/s).')
 parser.add_argument('--grid-parameters', type=str, help='Comma-separated file containing parameters for search grid. '
                                                   'First row is header with columns: '
                                                   'xmin,xmax,ymin,ymax,zmin,zmax,xstep,ystep,zstep. '
@@ -201,9 +206,11 @@ with open(args.arrival_time_file, 'r') as openfile:
         else:
             cols = row[:-1].split(',')
             arrival_time_data.append([])
-            arrival_time_data[-1].append(cols[0])
             for col in cols[1:]:
-                arrival_time_data[-1].append(datetime.datetime.strptime(col, '%Y-%m-%d %H:%M:%S'))
+                if int(col[:4]) < 1900:
+                    arrival_time_data[-1].append(float('nan'))
+                else:
+                    arrival_time_data[-1].append(datetime.datetime.strptime(col, '%Y-%m-%d %H:%M:%S'))
 
 network_data = []
 with open(args.network_file, 'r') as openfile:
@@ -244,18 +251,17 @@ if not args.grid_file:
     xmin, xmax, ymin, ymax, zmin, zmax, xstep, ystep, zstep = grid_parameters
 
     # Build travel time grid
-
-    grid_points = generate_tt_grid(xmin, xmax, ymin, ymax, zmin, zmax, xstep, ystep, zstep,
-                                   network_data, velocity_model)
+    grid_points, grid_header = generate_tt_grid(xmin, xmax, ymin, ymax, zmin, zmax, xstep, ystep, zstep,
+                                                network_data, velocity_model)
 
     # Save travel time grid to file
-
     with open('tt_grid.csv', 'w') as outfile:
         outstr = 'x,y,z,'
         for n in range(len(network_data)):
-            outstr += 'tt_' + network_data[n][0] + ','
+            outstr += 'ptt_' + network_data[n][0] + ',stt_' + network_data[n][0] + ','
         outstr = outstr[:-1] + '\n'
         outfile.write(outstr)
+        grid_header = outstr[:-1].split(',')  # Define the grid header in case the generated grid is used in this run
     with open('tt_grid.csv', 'a') as outfile:
         for i in range(len(grid_points)):
             for j in range(len(grid_points[i])):
@@ -269,7 +275,6 @@ if not args.grid_file:
 # Otherwise, build the travel time grid from a previously saved grid
 # Note: haven't tested this thoroughly
 else:
-    print(args.grid_file)
     with open(args.grid_file, 'r') as infile:
         header = -1
         gridx = []
@@ -278,6 +283,7 @@ else:
         travel_times = []
         for row in infile:
             if header == -1:
+                grid_header = row[:-1].split(',')
                 header = 0
             else:
                 cols = row.split(',')
@@ -297,3 +303,102 @@ else:
             for k in range(len(gridz)):
                 grid_points[i][j][k] = [gridx[i], gridy[j], gridz[k]]
                 grid_points[i][j][k].extend(travel_times[i + j + k])
+
+# Create a translation chart to find travel time data for a given column in the arrival time data.
+
+translation_chart = []
+for n in range(len(arrival_time_data_header)):
+    for m in range(len(grid_header)):
+        if m < 3:
+            continue
+        if ((arrival_time_data_header[n].split('_')[0] == grid_header[m].split('_')[1]) and
+                arrival_time_data_header[n].split('_')[1] == str.upper(grid_header[m][:1])):  # site and wave match
+            translation_chart.append(m)
+
+# Locate all events (only location method currently available is grid search)
+
+event_solutions = [[], [], [], [], [], [], [], []]
+for n in range(len(arrival_time_data)):
+    origin_grid = [[[[] for z in range(len(grid_points[0][0]))]
+                    for y in range(len(grid_points[0]))]
+                   for x in range(len(grid_points))]
+
+    # For each event, consider each grid cell as a possible location
+    for i in range(len(grid_points)):
+        for j in range(len(grid_points[i])):
+            for k in range(len(grid_points[i][j])):
+
+                # Calculate all origin times from the arrival time minus the travel time from the grid cell to each site
+                origin_times = []
+                max_weight = 0
+                weight_sum = 0
+                for m in range(len(arrival_time_data[n])):
+                    if arrival_time_data[m][n] != float('nan'):
+                        origin_times.append(arrival_time_data[m][n] - grid_points[i][j][k][translation_chart[n]])
+
+                # Calculate mean origin time and RMS error for the grid cell
+                mean_ot = np.mean(origin_times)
+                print(mean_ot)
+                rms = 0
+                for origin_time in origin_times:
+                    rms += (origin_time - mean_ot) ** 2
+                rms = math.sqrt(rms)
+
+                # Calculate weight, maximum weight, sum of weights for confidence interval determination
+                try:
+                    weight = 1 / (rms ** 2)
+                except:
+                    weight = math.inf  # Catch zero division in the case of 0 RMS error
+                if weight > max_weight:
+                    max_weight = weight
+                weight_sum += weight
+
+                # Save weight, RMS error and mean origin time
+                origin_grid[i][j][k] = [weight, rms, mean_ot]
+
+    # Using the weights defined, find the 95% confidence region
+    for c in np.linspace(0, 1, 100):
+        c_weight_sum = 0
+        ijk = []
+        for i in range(len(grid_points)):
+            for j in range(len(grid_points[i])):
+                for k in range(len(grid_points[i][j])):
+                    if origin_grid[i][j][k][0] > c * max_weight:
+                        ijk.append([i, j, k])
+                        c_weight_sum += origin_grid[i][j][k][0]
+        c_weight_sum /= weight_sum
+        P = 1 - c_weight_sum
+        if P > 0.95:
+            break
+
+    # Define centre and uncertainty of 95% confidence region: x,y,z and origin time
+    x, y, z, ots = [], [], [], []
+    if len(ijk) > 0:
+        for indices in ijk:
+            x.append(grid_points[indices[0]][indices[1]][indices[2]][0])
+            y.append(grid_points[indices[0]][indices[1]][indices[2]][1])
+            z.append(grid_points[indices[0]][indices[1]][indices[2]][2])
+            ots.append(mean_ot[indices[0]][indices[1]][indices[2]][2])
+    else:
+        print('No data exists in the 95% confidence region for this event.')
+    x_err = (max(x) - min(x)) / 2
+    y_err = (max(y) - min(y)) / 2
+    z_err = (max(z) - min(z)) / 2
+    ots_err = (max(ots) - min(ots)) / 2
+    mid_x = min(x) + x_err
+    mid_y = min(y) + y_err
+    mid_z = min(z) + z_err
+    mid_ot = min(ots) + ots_err
+
+    # Store solution and error
+    event_solutions[0] = mid_x
+    event_solutions[1] = mid_y
+    event_solutions[2] = mid_z
+    event_solutions[3] = mid_ot
+    event_solutions[4] = x_err
+    event_solutions[5] = y_err
+    event_solutions[6] = z_err
+    event_solutions[7] = ots_err
+
+
+
