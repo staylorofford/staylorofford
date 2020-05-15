0a8f3b1569fc0243c1bcb331f1089c374569c853
Sam Taylor-Offord <samto@hutl15444.gns.cri.nz>
2020-05-15 16:05:26 +1200

    Site noise script: add functionality to determine data completeness on sub-daily timescales and aggregate to a value over the input time period specified.

--- network_analysis/data_quality/seismic_site_data_quality.py
+++ network_analysis/data_quality/seismic_site_data_quality.py
@@ -3,6 +3,7 @@ Working script for seismic site data quality. Initially, this script will have f
 1. Query data over a given time window for site codes in a csv file, on the vertical channel,
 2. Filter data for a given filter(s),
 3. Calculate data completeness (% samples) over the time window, and produce average data values using the RMS.
+Note: while FDSN functionality was initially supported, further development has not continued to test this functionality.
 """
 
 # Import Python libraries
@@ -17,10 +18,8 @@ from obspy.core.stream import Stream
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.signal import PPSD
 from obspy import read
-from os import listdir
 from os import remove
 import pandas as pd
-import queue
 
 
 # Import my Python functions
@@ -51,15 +50,19 @@ def calculate_PPSD_noise(data, filter_type, minimum_frequency, maximum_frequency
 
     # Build probabilistic power spectral density objects for each trace
     client = Client("https://service.geonet.org.nz")
-    metadata = client.get_stations(network='NZ',
-                                   station=data[0].stats.station,
-                                   location=data[0].stats.location,
-                                   channel=data[0].stats.channel,
-                                   starttime=UTCDateTime(starttime),
-                                   endtime=UTCDateTime(endtime),
-                                   level='response')
-    ppsd = PPSD(data[0].stats, metadata)
-    ppsd.add(data[0])
+    try:
+        metadata = client.get_stations(network='NZ',
+                                       station=data.stats.station,
+                                       location=data.stats.location,
+                                       channel=data.stats.channel,
+                                       starttime=UTCDateTime(starttime),
+                                       endtime=UTCDateTime(endtime),
+                                       level='response')
+        ppsd = PPSD(data.stats, metadata)
+        ppsd.add(data)
+    except FDSNNoDataException:
+        # When no response data exists
+        return np.nan, np.nan
 
     # Find RMS value from PPSD.
     # 1) Take the mean value of PPSD in given frequency window as the PSD value
@@ -110,14 +113,7 @@ def query_fdsn(station, location, channel, starttime, endtime, client="https://s
     return stream
 
 
-def data_quality(site_code, location_code, latitude, longitude):
-
-    """
-    Perform data quality calculation. All variables not set in q are considered global and nonvariant.
-    Output is saved to files.
-    :param q: list containing station code and location code to process.
-    :return: none
-    """
+def data_quality(site_code, location_code, channel_code, latitude, longitude, starttime, endtime, splittime=None):
 
     try:
         if int(location_code) >= 20:
@@ -125,7 +121,8 @@ def data_quality(site_code, location_code, latitude, longitude):
     except ValueError:
         return None
 
-    print('Downloading data for station ' + site_code + ' location code ' + location_code + '...')
+    print('Downloading data for station ' + site_code + ' location code ' + location_code + ' channel code '
+          + channel_code + " over time " + starttime.isoformat() + ' - ' + endtime.isoformat() + '...')
 
     if data_source == 'FDSN':
         # Query data from FDSN
@@ -193,6 +190,8 @@ def data_quality(site_code, location_code, latitude, longitude):
             # Save non-results
 
             outstr = (site_code + ',' +
+                      location_code + ',' +
+                      channel_code + ',' +
                       latitude + ',' +
                       longitude + ',' +
                       starttime.isoformat() + ',' +
@@ -200,6 +199,7 @@ def data_quality(site_code, location_code, latitude, longitude):
                       filter_type + ',' +
                       str(minimum_frequency) + ',' +
                       str(maximum_frequency) + ',' +
+                      'NA' + ',' +
                       str(np.nan) + '\n')
             return outstr
 
@@ -208,19 +208,21 @@ def data_quality(site_code, location_code, latitude, longitude):
         # Download data
 
         possible_keys = create_miniseed_key_list([site_code], [location_code], [channel_code], starttime, endtime)
+        files = []
         for file_key in possible_keys:
             try:
                 client.download_file('geonet-archive',
                                      file_key,
                                      file_key.split('/')[-1])
                 print(file_key + ' downloaded successfully.')
+                files.append(file_key.split('/')[-1])
             except:  # Fails if the file doesn't exist
                 print(file_key + ' failed to be downloaded.')
                 pass
 
     print('Data download complete. Calculating data quality...')
 
-    # Calculate data completeness, RMS value
+    # Calculate data on/off times, RMS values
 
     if data_source == 'FDSN':
 
@@ -228,26 +230,22 @@ def data_quality(site_code, location_code, latitude, longitude):
 
     elif data_source == 'S3':
 
-        # Here completeness and RMS values are taken as averages of daily values. We assume that the number of
-        # samples in each day is approximately the same, making this a statistically valid approach. Daily averaging
-        # is done due to the large data volumes encountered using this data source.
+        # Here RMS values are taken as mean spectral intensity values in each band of a PPSD
 
-        files = listdir('.')
-        RMSs, ppsds = [], []
+        RMSs, times = [], []
         for file in files:
-            if file[-1:] == 'D' and '.' + str(site_code) + '.' in file and '.' + str(location_code) + '-' in file:
-                print('Calculating data quality for file ' + file)
-                data = read(file)
-                daily_RMS, ppsd = calculate_PPSD_noise(data, filter_type, minimum_frequency, maximum_frequency,
-                                                       starttime, endtime)
-                if np.isnan(daily_RMS):
+            print('Calculating data quality for file ' + file)
+            data = read(file)
+            # Iterative over data if there are gaps
+            for data_chunk in data:
+                RMS, ppsd = calculate_PPSD_noise(data_chunk, filter_type, minimum_frequency, maximum_frequency,
+                                                 starttime, endtime)
+                if np.isnan(RMS):
                     continue
-                RMSs.append(daily_RMS)
-                # PPSD: need to find a way to compare the noise value returned from this (which is what, a mean?)
-                # to the RMS value determined from timeseries data. May require some digging into literature, but
-                # have timeseries mean value for comparison.
-                ppsds.append(ppsd)
-                remove(file)
+                RMSs.append(RMS)
+                chunk_st, chunk_et = ppsd.times_data[0]
+                times.append([chunk_st, chunk_et])
+            remove(file)
 
         if len(RMSs) == 0:
             print('There was no data, moving onto next station.')
@@ -255,6 +253,8 @@ def data_quality(site_code, location_code, latitude, longitude):
             # Save non-results
 
             outstr = (site_code + ',' +
+                      location_code + ',' +
+                      channel_code + ',' +
                       latitude + ',' +
                       longitude + ',' +
                       starttime.isoformat() + ',' +
@@ -262,40 +262,48 @@ def data_quality(site_code, location_code, latitude, longitude):
                       filter_type + ',' +
                       str(minimum_frequency) + ',' +
                       str(maximum_frequency) + ',' +
+                      '0' + ',' +
                       str(np.nan) + '\n')
             return outstr
 
-        # Calculate mean completeness, RMS
-        mean_RMS = np.nanmean(RMSs)  # The mean RMS is the mean of daily RMS values
-        print('Data quality calculation complete for data from site ' + site_code + ' location code ' +
-              location_code + ' channel code ' + channel_code)
-        print('Mean RMS is: ' + str(mean_RMS))
+        else:
 
-    # Save results
+            # Save results
 
-    outstr=(site_code + ',' +
-            latitude + ',' +
-            longitude + ',' +
-            starttime.isoformat() + ',' +
-            endtime.isoformat() + ',' +
-            filter_type + ',' +
-            str(minimum_frequency) + ',' +
-            str(maximum_frequency) + ',' +
-            str(mean_RMS)[:6] + '\n')
-    return outstr
+            outstr = ''
+            for idx, time in enumerate(times):
+
+                outstr += (site_code + ',' +
+                           location_code + ',' +
+                           channel_code + ',' +
+                           latitude + ',' +
+                           longitude + ',' +
+                           time[0].isoformat() + ',' +
+                           time[1].isoformat() + ',' +
+                           filter_type + ',' +
+                           str(minimum_frequency) + ',' +
+                           str(maximum_frequency) + ',' +
+                           str(RMSs[idx])[:6] + '\n')
+
+            print('Data quality calculation complete for data from site ' + site_code + ' location code ' +
+                  location_code + ' channel code ' + channel_code + ' over time ' + starttime.isoformat() + ' - ' +
+                  endtime.isoformat())
+            return outstr
 
 
 # Give the path to the site file: a csv containing in each row the station code and location code of each site to use
 # in the data quality calculations. The station codes should be in the first column of this file, and the location
 # code can be in any other column so long as the file header specifies which by the location of the "Location" string
 # in the header.
-site_file = '/home/samto/git/delta/network/sites.csv'
+# site_file = '/home/samto/git/delta/network/sites.csv'
+site_file = './sites'
+# site_file = './sites.csv'
 
 # Give parameters for which data to query
-channel_code = 'HHZ'  # Channel code to query
-starttime = '2019-01-01T00:00:00Z'  # starttime can either be an ISO8601 string, or an integer number of seconds to
+channel_codes = ['EHZ', 'HHZ']  # Channel code(s) to query, if only one is desired have a list with one entry
+starttime = '2019-03-06T00:00:00Z'  # starttime can either be an ISO8601 string, or an integer number of seconds to
                                     # query data for before the endtime.
-endtime = '2019-01-10T00:00:01Z'  # endtime can be an ISO8601 string or 'now', if 'now' then endtime
+endtime = '2020-12-31T00:00:01Z'  # endtime can be an ISO8601 string or 'now', if 'now' then endtime
                                   # will be set to the current UTC time. If using S3 data_source, make endtime at least
                                   # 1 second into the final day of data you want to use.
 
@@ -328,10 +336,6 @@ else:
 # Parse site code metadata from file
 site_metadata = pd.read_csv(site_file, header=0, index_col=0)
 
-# Prepare output file
-with open('seismic_site_data_quality_output.csv', 'w') as outfile:
-    outfile.write('Station,Longitude,Latitude,Starttime,Endtime,Filter,Freqmin,Freqmax,RMS\n')
-
 # Prepare data query variables depending on data_source
 if data_source == 'FDSN':
     # Determine which FDSN client to use
@@ -357,24 +361,33 @@ else:
 # Pump parameters into parallel processing pool and do data quality calculation
 
 # Prepare the workers for parallel processing
-pp_pool = multiprocessing.Pool()
+pp_pool = multiprocessing.Pool(3)
 
 jobs = []
-for m in range(len(site_metadata)):
 
-    # Get site information
-    site_code = site_metadata.index[m]
-    location_code = site_metadata.iloc[m]['Location']
-    latitude = str(site_metadata.iloc[m]['Longitude'])
-    longitude = str(site_metadata.iloc[m]['Latitude'])
+for l in range(math.ceil((endtime - starttime).total_seconds() / 86400)):
+    # Increment the date
+    date = starttime + datetime.timedelta(days=l)
 
-    # Send jobs to the pool of workers
-    job = pp_pool.apply_async(data_quality, (site_code, location_code, latitude, longitude))
-    jobs.append(job)
+    for m in range(len(site_metadata)):
+
+        # Get site information
+        site_code = str(site_metadata.index[m])
+        location_code = str(site_metadata.iloc[m]['Location'])
+        latitude = str(site_metadata.iloc[m]['Longitude'])
+        longitude = str(site_metadata.iloc[m]['Latitude'])
+
+        for n in range(len(channel_codes)):
+
+            # Send jobs to the pool of workers
+            job = pp_pool.apply_async(data_quality, (site_code, location_code, channel_codes[n], latitude, longitude,
+                                                     date, date + datetime.timedelta(days=1, microseconds=-1)))
+            jobs.append(job)
 
 # Collect results from the workers through the pool result queue
 with open('seismic_site_data_quality_output.csv', 'w') as outfile:
     # ^ IMPORTANT: have to use 'w' when working with parallel processes even though 'a' behaviour is desired!
+    outfile.write('Station,Location,Channel,Longitude,Latitude,Starttime,Endtime,Filter,Freqmin,Freqmax,RMS\n')
     for job in jobs:
         result = job.get()
         try:
@@ -387,3 +400,62 @@ with open('seismic_site_data_quality_output.csv', 'w') as outfile:
 
 pp_pool.close()
 pp_pool.join()
+
+# Aggregate results per site
+
+with open('seismic_site_data_quality_summary.csv', 'w') as outfile:
+    outfile.write('Station,Location,Channel,Longitude,Latitude,Starttime,Endtime,Filter,Freqmin,Freqmax,Completeness,'
+                  'RMS\n')
+with open('seismic_site_data_quality_summary.csv', 'a') as outfile:
+    # Load in input file and parse it into rows for each channel at each site
+    all_site_rows = [[[] for m in range(len(channel_codes))] for n in range(len(site_metadata))]
+    for n, site in enumerate(site_metadata.index):
+        for m, channel in enumerate(channel_codes):
+            with open('seismic_site_data_quality_output.csv', 'r') as infile:
+                for row in infile:
+                    cols = row.split(',')
+                    if cols[0] == site and cols[2] == channel:
+                        all_site_rows[n][m].append(row)
+        # Then, aggregate all the different rows into a summary row for each channel at each site. This collapses
+        # the data by time.
+        channel_rows = [[] for m in range(len(channel_codes))]
+        for site_rows in all_site_rows:
+            for channel_rows in site_rows:
+                try:
+                    first_row = channel_rows[0].split(',')
+                except IndexError:
+                    continue
+                site = first_row[0]
+                location_code = first_row[1]
+                longitude = first_row[3]
+                latitude = first_row[4]
+                for channel in channel_codes:
+                    mean_RMS = 0
+                    mean_completeness = 0
+                    numentries = 0
+                    for row in channel_rows:
+                        cols = row.split(',')
+                        if cols[2] == channel:
+                            if cols[-1][:3] != 'nan':
+                                mean_RMS += float(cols[-1])
+                                mean_completeness += (datetime.datetime.strptime(cols[6][:19], '%Y-%m-%dT%H:%M:%S') -
+                                                      datetime.datetime.strptime(cols[5][:19], '%Y-%m-%dT%H:%M:%S')
+                                                      ).total_seconds()
+                                numentries += 1
+                    if numentries > 0:
+                        mean_RMS /= numentries
+                    else:
+                        mean_RMS = np.nan
+                    mean_completeness /= (endtime - starttime).total_seconds()
+                    outfile.write(site + ',' +
+                                  location_code + ',' +
+                                  channel + ',' +
+                                  longitude + ',' +
+                                  latitude + ',' +
+                                  starttime.isoformat() + ',' +
+                                  endtime.isoformat() + ',' +
+                                  filter_type + ',' +
+                                  str(minimum_frequency) + ',' +
+                                  str(maximum_frequency) + ',' +
+                                  str(mean_completeness) + ',' +
+                                  str(mean_RMS) + '\n')
