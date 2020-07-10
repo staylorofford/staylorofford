697a28762673e3be77c0b1286031019159e93fa3
Sam Taylor-Offord <samto@hutl15444.gns.cri.nz>
2020-07-10 15:10:27 +1200

    site_noise: add functionality to calculate RMS values for locally stored files

--- network_analysis/data_quality/seismic_site_data_quality.py
+++ network_analysis/data_quality/seismic_site_data_quality.py
@@ -8,6 +8,7 @@ Note: while FDSN functionality was initially supported, further development has 
 
 # Import Python libraries
 import datetime
+import io
 import math
 import matplotlib.pyplot as plt
 import multiprocessing
@@ -16,10 +17,11 @@ from obspy.clients.fdsn import Client
 from obspy.clients.fdsn.header import FDSNNoDataException
 from obspy.core.stream import Stream
 from obspy.core.utcdatetime import UTCDateTime
-from obspy.signal import PPSD
 from obspy import read
 from os import remove
+from os import walk
 import pandas as pd
+import pycurl
 
 
 # Import my Python functions
@@ -29,17 +31,17 @@ from assume_role import assume_role
 from create_key_list import create_miniseed_key_list
 
 
-def calculate_PPSD_noise(data, filter_type, minimum_frequency, maximum_frequency, starttime, endtime):
+def calculate_noise(data, filter_type, minimum_frequency, maximum_frequency, starttime, endtime):
 
     """
-    Calculate data quality via data completeness and RMS value.
+    Calculate average noise for a seismic site in the recorded units.
     :param data: obspy Stream object containing single trace to calculate data quality for
     :param filter_type: obspy filter type
     :param minimum_frequency: minimum frequency to use in filter
     :param maximum_frequency: maximum frequency to use in filter
     :param starttime: start time of data query, for FDSN completeness calculation, as ISO8601 string
     :param endtime: end time of data query, for FDSN completeness calculation, as ISO8601 string
-    :return: data RMS value, number of data values
+    :return: data RMS value
     """
 
     # Apply a filter to the data
@@ -48,45 +50,40 @@ def calculate_PPSD_noise(data, filter_type, minimum_frequency, maximum_frequency
                            freqmin=minimum_frequency,
                            freqmax=maximum_frequency)
 
-    # Build probabilistic power spectral density objects for each trace
-    client = Client("https://service.geonet.org.nz")
+    # Scale data into nm/s
+    gain = query_gain(data.stats.station, data.stats.location, data.stats.channel, starttime, endtime)
     try:
-        metadata = client.get_stations(network='NZ',
-                                       station=data.stats.station,
-                                       location=data.stats.location,
-                                       channel=data.stats.channel,
-                                       starttime=UTCDateTime(starttime),
-                                       endtime=UTCDateTime(endtime),
-                                       level='response')
-        ppsd = PPSD(data.stats, metadata)
-        ppsd.add(data)
-    except FDSNNoDataException:
-        # When no response data exists
-        return np.nan, np.nan
+        gain = float(gain)
+        data.data = gain * data.data
 
-    # Find RMS value from PPSD.
-    # 1) Take the mean value of PPSD in given frequency window as the PSD value
-    # 2) Calculate weighted mean of PSD values in all windows using frequency window width as weights and scaling the
-    # acceleration squared values by the window centre frequency squared to convert the result into velocity squared.
-    # Also convert the data values out of dB scale as precursor to this.
-    # 3) Take sqrt of weighted mean, as data are squared when processed to produce PSD. This gives the RMS value.
-    weighted_mean, weight_sum = 0, 0
+        # Calculate RMS value
+        RMS = np.sqrt(np.mean(data.data ** 2))
 
-    try:
-        _, mean_psds = ppsd.get_mean()
-    except Exception:
-        # Fails when no data exists
-        return np.nan, np.nan
+    except ValueError:
+        print('Failed to find sensor gain from FDSN! No scaling will be applied to RMS value for station ' +
+              data.stats.station + ', location ' + data.stats.location + ', channel ' + data.stats.channel +
+              ', times ' + starttime.isoformat() + '-' + endtime.isoformat() + '. Will return NaN value for RMS.')
+        RMS = np.nan
 
-    psd_widths = [1/ppsd.period_bin_left_edges[n] - 1/ppsd.period_bin_right_edges[n]
-                  for n in range(len(ppsd.period_bin_left_edges))]
-    psd_centres = [1/ppsd.period_bin_centers[n] for n in range(len(ppsd.period_bin_centers))]
-    for n in range(len(mean_psds)):
-        weighted_mean += math.sqrt(10**(mean_psds[n] / 10) / (psd_centres[n] ** 2)) * psd_widths[n]
-        weight_sum += psd_widths[n]
-    weighted_mean /= weight_sum
+    return RMS
 
-    return weighted_mean, ppsd
+
+def curl(curlstr):
+
+    """
+    Perform curl with curlstr
+    :param curlstr: string to curl
+    :return: curl output
+    """
+
+    buffer = io.BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, curlstr)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+
+    return buffer.getvalue()
 
 
 def query_fdsn(station, location, channel, starttime, endtime, client="https://service.geonet.org.nz"):
@@ -113,7 +110,35 @@ def query_fdsn(station, location, channel, starttime, endtime, client="https://s
     return stream
 
 
-def data_quality(site_code, location_code, channel_code, latitude, longitude, starttime, endtime, splittime=None):
+def query_gain(station, location, channel, starttime, endtime):
+
+    """
+    Query FDSN for site metadata.
+    :param: station: site code to get data from (string)
+    :param: location: location code to get data from (string)
+    :param: channel: channel code to get data from (string)
+    :param: starttime: UTC start time of desired data (ISO8601 string)
+    :param: endtime: UTC end time of desired data (ISO8601 string)
+    :param: client: FDSN webservice to use as the client:
+            https://service.geonet.org.nz (archive) or https://service-nrt.geonet.org.nz (last week)
+    :return: gain value for site
+    """
+
+    query_string = 'https://service.geonet.org.nz/fdsnws/station/1/query?station=' + station + \
+                   '&location=' + str(location) + \
+                   '&channel=' + str(channel) + \
+                   '&starttime=' + starttime.isoformat() + \
+                   '&endtime=' + endtime.isoformat() + \
+                   '&level=channel'
+    curl_result = curl(query_string).decode('ascii')
+    sti = curl_result.find('<InstrumentSensitivity><Value>')
+    ste = curl_result.find('</Value>', sti)
+    gain = curl_result[sti + len('<InstrumentSensitivity><Value>'):ste]
+    return gain
+
+
+def data_quality(site_code, location_code, channel_code, latitude, longitude, data_source, starttime, endtime,
+                 splittime=None):
 
     try:
         if int(location_code) >= 20:
@@ -199,9 +224,25 @@ def data_quality(site_code, location_code, channel_code, latitude, longitude, st
                       filter_type + ',' +
                       str(minimum_frequency) + ',' +
                       str(maximum_frequency) + ',' +
-                      'NA' + ',' +
                       str(np.nan) + '\n')
             return outstr
+
+    elif data_source == 'local':
+
+        # Find files matching the site, location, and channel codes
+
+        found_files = []
+        for root, dirs, files in walk(day_file_directory_root):
+            for file in files:
+                try:
+                    st = read(root + '/' + file)
+                    if (st[0].stats.station == site_code and
+                            st[0].stats.location == location_code and
+                            st[0].stats.channel == channel_code):
+                        found_files.append(root + '/' + file)
+                except:
+                    continue
+        files = found_files
 
     elif data_source == 'S3':
 
@@ -226,11 +267,11 @@ def data_quality(site_code, location_code, channel_code, latitude, longitude, st
 
     if data_source == 'FDSN':
 
-        mean_RMS, _ = calculate_PPSD_noise(data, filter_type, minimum_frequency, maximum_frequency, starttime, endtime)
+        mean_RMS, _ = calculate_noise(data, filter_type, minimum_frequency, maximum_frequency, starttime, endtime)
 
-    elif data_source == 'S3':
+    elif data_source == 'local' or 'S3':
 
-        # Here RMS values are taken as mean spectral intensity values in each band of a PPSD
+        # Here daily RMS values will be calculated for each site/location/channel code combination
 
         RMSs, times = [], []
         for file in files:
@@ -238,13 +279,10 @@ def data_quality(site_code, location_code, channel_code, latitude, longitude, st
             data = read(file)
             # Iterative over data if there are gaps
             for data_chunk in data:
-                RMS, ppsd = calculate_PPSD_noise(data_chunk, filter_type, minimum_frequency, maximum_frequency,
-                                                 starttime, endtime)
-                if np.isnan(RMS):
-                    continue
+                RMS = calculate_noise(data_chunk, filter_type, minimum_frequency, maximum_frequency,
+                                      starttime, endtime)
                 RMSs.append(RMS)
-                chunk_st, chunk_et = ppsd.times_data[0]
-                times.append([chunk_st, chunk_et])
+                times.append([data_chunk.stats.starttime, data_chunk.stats.endtime])
             remove(file)
 
         if len(RMSs) == 0:
@@ -262,7 +300,6 @@ def data_quality(site_code, location_code, channel_code, latitude, longitude, st
                       filter_type + ',' +
                       str(minimum_frequency) + ',' +
                       str(maximum_frequency) + ',' +
-                      '0' + ',' +
                       str(np.nan) + '\n')
             return outstr
 
@@ -296,20 +333,22 @@ def data_quality(site_code, location_code, channel_code, latitude, longitude, st
 # code can be in any other column so long as the file header specifies which by the location of the "Location" string
 # in the header.
 # site_file = '/home/samto/git/delta/network/sites.csv'
-site_file = './sites'
-# site_file = './sites.csv'
+site_file = '/home/samto/PROCESSING/sites.csv'
 
 # Give parameters for which data to query
 channel_codes = ['EHZ', 'HHZ']  # Channel code(s) to query, if only one is desired have a list with one entry
-starttime = '2019-03-06T00:00:00Z'  # starttime can either be an ISO8601 string, or an integer number of seconds to
+starttime = '2020-01-01T00:00:00Z'  # starttime can either be an ISO8601 string, or an integer number of seconds to
                                     # query data for before the endtime.
-endtime = '2020-12-31T00:00:01Z'  # endtime can be an ISO8601 string or 'now', if 'now' then endtime
+endtime = '2020-12-31T23:59:59Z'  # endtime can be an ISO8601 string or 'now', if 'now' then endtime
                                   # will be set to the current UTC time. If using S3 data_source, make endtime at least
                                   # 1 second into the final day of data you want to use.
 
-# Set whether to get data from FDSN (short duration) or S3. If S3 data will be downloaded for all full days between
-# starttime and endtime inclusive.
+# Set whether to get data from FDSN (short duration), local or S3. If S3 data will be downloaded for all full days
+# between starttime and endtime inclusive.
 data_source = 'S3'
+
+# If you set data_source = 'local', provide path details to root folder containing all daily miniSEED files
+day_file_directory_root = '/mnt/hgfs/VMSHARE/CRSZ testing/'
 
 # If you set data_source = 'S3', provide role details for S3 use
 role = 'arn:aws:iam::862640294325:role/tf-sod-team-s3-read-role'
@@ -317,7 +356,7 @@ user = 'arn:aws:iam::582058524534:mfa/samto'
 duration = 43200
 
 # Set how to filter the data, if at all. Use filter_type=None to negate filtering. Filter types are those in obspy.
-filter_type = 'bandpass'
+filter_type = None
 minimum_frequency = 2
 maximum_frequency = 15
 
@@ -355,13 +394,13 @@ elif data_source == 'S3':
     client = assume_role(role, user, duration)
 
 else:
-    print('data_source needs to be set to either \'FDSN\' or \'S3\' for the script to perform work.')
+    print('data_source needs to be set to either \'FDSN\', \'local\', or \'S3\' for the script to perform work.')
     exit()
 
 # Pump parameters into parallel processing pool and do data quality calculation
 
 # Prepare the workers for parallel processing
-pp_pool = multiprocessing.Pool(3)
+pp_pool = multiprocessing.Pool(25)
 
 jobs = []
 
@@ -381,7 +420,8 @@ for l in range(math.ceil((endtime - starttime).total_seconds() / 86400)):
 
             # Send jobs to the pool of workers
             job = pp_pool.apply_async(data_quality, (site_code, location_code, channel_codes[n], latitude, longitude,
-                                                     date, date + datetime.timedelta(days=1, microseconds=-1)))
+                                                     data_source, date,
+                                                     date + datetime.timedelta(days=1, microseconds=-1)))
             jobs.append(job)
 
 # Collect results from the workers through the pool result queue
@@ -408,54 +448,37 @@ with open('seismic_site_data_quality_summary.csv', 'w') as outfile:
                   'RMS\n')
 with open('seismic_site_data_quality_summary.csv', 'a') as outfile:
     # Load in input file and parse it into rows for each channel at each site
-    all_site_rows = [[[] for m in range(len(channel_codes))] for n in range(len(site_metadata))]
-    for n, site in enumerate(site_metadata.index):
-        for m, channel in enumerate(channel_codes):
-            with open('seismic_site_data_quality_output.csv', 'r') as infile:
-                for row in infile:
-                    cols = row.split(',')
-                    if cols[0] == site and cols[2] == channel:
-                        all_site_rows[n][m].append(row)
-        # Then, aggregate all the different rows into a summary row for each channel at each site. This collapses
-        # the data by time.
-        channel_rows = [[] for m in range(len(channel_codes))]
-        for site_rows in all_site_rows:
-            for channel_rows in site_rows:
-                try:
-                    first_row = channel_rows[0].split(',')
-                except IndexError:
-                    continue
-                site = first_row[0]
-                location_code = first_row[1]
-                longitude = first_row[3]
-                latitude = first_row[4]
-                for channel in channel_codes:
-                    mean_RMS = 0
-                    mean_completeness = 0
-                    numentries = 0
-                    for row in channel_rows:
-                        cols = row.split(',')
-                        if cols[2] == channel:
-                            if cols[-1][:3] != 'nan':
-                                mean_RMS += float(cols[-1])
-                                mean_completeness += (datetime.datetime.strptime(cols[6][:19], '%Y-%m-%dT%H:%M:%S') -
-                                                      datetime.datetime.strptime(cols[5][:19], '%Y-%m-%dT%H:%M:%S')
-                                                      ).total_seconds()
-                                numentries += 1
-                    if numentries > 0:
-                        mean_RMS /= numentries
-                    else:
-                        mean_RMS = np.nan
-                    mean_completeness /= (endtime - starttime).total_seconds()
-                    outfile.write(site + ',' +
-                                  location_code + ',' +
-                                  channel + ',' +
-                                  longitude + ',' +
-                                  latitude + ',' +
-                                  starttime.isoformat() + ',' +
-                                  endtime.isoformat() + ',' +
-                                  filter_type + ',' +
-                                  str(minimum_frequency) + ',' +
-                                  str(maximum_frequency) + ',' +
-                                  str(mean_completeness) + ',' +
-                                  str(mean_RMS) + '\n')
+    entries = []
+    with open('seismic_site_data_quality_output.csv', 'r') as infile:
+        for row in infile:
+            if ',nan' not in row and 'Station' not in row:
+                cols = row.split(',')
+                entries.append(cols[0] + ',' + cols[1] + ',' + cols[2] + ',' + cols[3] + ',' + cols[4])
+        entries = list(set(entries))
+        completenesses = [0] * len(entries)
+        RMSs = [0] * len(entries)
+        numentries = [0] * len(entries)
+    with open('seismic_site_data_quality_output.csv', 'r') as infile:
+        for row in infile:
+            if ',nan' not in row and 'Station' not in row:
+                cols = row.split(',')
+                idx = entries.index(cols[0] + ',' + cols[1] + ',' + cols[2] + ',' + cols[3] + ',' + cols[4])
+                completenesses[idx] += (datetime.datetime.strptime(cols[6][:19], '%Y-%m-%dT%H:%M:%S') -
+                                        datetime.datetime.strptime(cols[5][:19], '%Y-%m-%dT%H:%M:%S')
+                                        ).total_seconds()
+                RMSs[idx] += float(cols[-1])
+                numentries[idx] += 1
+        for n in range(len(entries)):
+            completenesses[n] /= (endtime - starttime).total_seconds()
+            if numentries[n] > 0:
+                RMSs[n] /= numentries[n]
+            else:
+                RMSs[n] = np.nan
+            outfile.write(entries[n] + ',' +
+                          starttime.isoformat() + ',' +
+                          endtime.isoformat() + ',' +
+                          filter_type + ',' +
+                          str(minimum_frequency) + ',' +
+                          str(maximum_frequency) + ',' +
+                          str(completenesses[n]) + ',' +
+                          str(RMSs[n]) + '\n')
